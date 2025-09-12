ac49c8741297f5ec5412c15826862d431b4edd9b
Gift Ilocie <gilocie@gmail.com>
2025-09-12 16:56:13 +0000

    Hight of Performance Record to mach of Team Collaboration

--- src/components/dashboard/project-analytics-chart.tsx
+++ src/components/dashboard/project-analytics-chart.tsx
@@ -41,7 +41,7 @@ export function ProjectAnalyticsChart() {
         </CardDescription>
       </CardHeader>
       <CardContent className="pl-2">
-        <ResponsiveContainer width="100%" height={250}>
+        <ResponsiveContainer width="100%" height={265}>
           <BarChart data={data}>
             <XAxis
               dataKey="name"
