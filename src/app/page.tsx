e175f73c5d9a922723939365ac607b5669f3a3d8
Gift Ilocie <gilocie@gmail.com>
2025-09-12 19:14:05 +0000

    Then reduce chat box card to mach level with notice board

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -221,7 +221,7 @@ export default function Home() {
             </div>
           </div>
           <div className="grid grid-cols-10 gap-4">
-            <div className="col-span-10 lg:col-span-7 h-full md:h-[500px]">
+            <div className="col-span-10 lg:col-span-7 h-full md:h-[430px]">
                 <GoalReaderAIChat />
             </div>
             <div className="col-span-10 lg:col-span-3 grid grid-cols-1 gap-4 auto-rows-min">
