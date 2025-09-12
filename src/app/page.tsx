4f75c065a299e6786392a1b1993d15e59cc8e082
Gift Ilocie <gilocie@gmail.com>
2025-09-12 19:09:06 +0000

    Lets make those section to be same level below

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -221,10 +221,10 @@ export default function Home() {
             </div>
           </div>
           <div className="grid grid-cols-10 gap-4">
-            <div className="col-span-10 lg:col-span-7 h-full md:h-[500px]">
+            <div className="col-span-10 lg:col-span-7 h-full">
                 <GoalReaderAIChat />
             </div>
-            <div className="col-span-10 lg:col-span-3 grid grid-cols-1 gap-4 auto-rows-min">
+            <div className="col-span-10 lg:col-span-3 grid grid-cols-1 gap-4 auto-rows-fr">
               <Reminders />
               <Noticeboard />
             </div>
