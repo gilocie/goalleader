a411a1050563ca8d66ba0e731f48d37171ac90e9
Gift Ilocie <gilocie@gmail.com>
2025-09-14 15:12:14 +0000

    when chat mode is closed lets names fill the entire space as well

--- src/components/chat/chat-layout.tsx
+++ src/components/chat/chat-layout.tsx
@@ -3,7 +3,6 @@
 
 import { ChatContactList } from './chat-contact-list';
 import { ChatMessages } from './chat-messages';
-import { ChatUserProfile } from './chat-user-profile';
 import { Contact, Message } from '@/types/chat';
 
 interface ChatLayoutProps {
@@ -30,24 +29,17 @@ export function ChatLayout({ contacts, messages, selectedContact, onSelectContac
 
   return (
     <div className="grid grid-cols-10 h-full w-full">
-      <div className="col-span-10 md:col-span-3 lg:col-span-2 border-r">
+      <div className="col-span-10 md:col-span-4 lg:col-span-3 border-r">
         <ChatContactList 
           contacts={contacts} 
           onSelectContact={onSelectContact}
         />
       </div>
-      <div className="col-span-10 md:col-span-7 lg:col-span-5 hidden md:flex flex-col">
+      <div className="col-span-10 md:col-span-6 lg:col-span-7 hidden md:flex flex-col">
         {/* Placeholder for when no chat is selected */}
         <div className="flex flex-col items-center justify-center h-full text-center text-muted-foreground">
           <p className="text-lg font-medium">Select a contact to start chatting</p>
           <p className="text-sm">Your conversations will appear here.</p>
-        </div>
-      </div>
-      <div className="col-span-10 lg:col-span-3 border-l hidden lg:flex flex-col">
-        {/* Placeholder for user profile */}
-        <div className="flex flex-col items-center justify-center h-full text-center text-muted-foreground">
-            <p className="text-lg font-medium">User Profile</p>
-            <p className="text-sm">Contact details will show up here.</p>
         </div>
       </div>
     </div>
