8a46557a6b79d55532e08c7356b0eddbbaf77bd1
Gift Ilocie <gilocie@gmail.com>
2025-09-26 12:26:08 +0000

    now lets do the same with register page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -7,7 +7,7 @@ import { Label } from '@/components/ui/label';
 import { Logo } from '@/components/icons';
 import Link from 'next/link';
 
-export default function LoginPage() {
+export default function RegisterPage() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-muted/40">
       <Card className="w-full max-w-sm">
@@ -15,32 +15,31 @@ export default function LoginPage() {
             <div className="flex justify-center pb-4">
                 <Logo className="h-10 w-10 text-primary" />
             </div>
-          <CardTitle className="text-2xl">Welcome Back</CardTitle>
-          <CardDescription>Enter your email below to login to your account</CardDescription>
+          <CardTitle className="text-2xl">Create an account</CardTitle>
+          <CardDescription>Enter your information to create an account</CardDescription>
         </CardHeader>
         <CardContent className="grid gap-4">
+          <div className="grid gap-2">
+              <Label htmlFor="full-name">Full name</Label>
+              <Input id="full-name" placeholder="Patrick Achitabwino" required />
+            </div>
           <div className="grid gap-2">
             <Label htmlFor="email">Email</Label>
             <Input id="email" type="email" placeholder="m@example.com" required />
           </div>
           <div className="grid gap-2">
-            <div className="flex items-center">
-              <Label htmlFor="password">Password</Label>
-              <Link href="#" className="ml-auto inline-block text-sm underline">
-                Forgot your password?
-              </Link>
-            </div>
+            <Label htmlFor="password">Password</Label>
             <Input id="password" type="password" required />
           </div>
           <Button type="submit" className="w-full bg-gradient-to-r from-primary to-green-700 text-primary-foreground hover:from-primary/90 hover:to-green-700/90">
-            Login
+            Create account
           </Button>
         </CardContent>
         <CardFooter className="flex flex-col gap-4">
           <div className="text-center text-sm">
-            Don&apos;t have an account?{' '}
-            <Link href="/register" className="underline">
-              Sign up
+            Already have an account?{' '}
+            <Link href="/login" className="underline">
+              Login
             </Link>
           </div>
         </CardFooter>
