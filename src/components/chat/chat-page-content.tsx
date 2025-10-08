296588188a22609f89c6a64c79f82e74310a7efa
Gift Ilocie <gilocie@gmail.com>
2025-10-08 06:38:23 +0000

    Perfect lets add 40px and see?

--- src/components/chat/chat-page-content.tsx
+++ src/components/chat/chat-page-content.tsx
@@ -34,7 +34,7 @@ export function ChatPageContent() {
   }
 
   return (
-    <div className="flex-1 w-full overflow-hidden md:max-h-[450px]">
+    <div className="flex-1 w-full overflow-hidden md:max-h-[490px]">
       <ChatLayout
         contacts={contacts}
         messages={contactMessages}
