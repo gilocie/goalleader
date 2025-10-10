591bc00f881618bb52c16f47a606aa85a4520f48
Gift Ilocie <gilocie@gmail.com>
2025-10-10 04:08:36 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,5 +1,10 @@
 // This file is intentionally left blank. 
 // The Firebase config will be populated by the Firebase Studio backend.
-export const firebaseConfig = {};
-
-    
+export const firebaseConfig = {
+  "projectId": "studio-2182003100-d9519",
+  "appId": "1:364926480587:web:9af720b133167287e0eb4e",
+  "apiKey": "AIzaSyAUMQ0YQmgMbqdkeJQdVsZ5CxgM1Bodh4A",
+  "authDomain": "studio-2182003100-d9519.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "364926480587"
+};
