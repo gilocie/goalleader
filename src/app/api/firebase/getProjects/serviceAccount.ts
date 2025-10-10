083e810fc2af308086bdb13f06c9441c3bece3f3
Gift Ilocie <gilocie@gmail.com>
2025-10-10 11:46:59 +0000

    you know the problem, at first i went to firebase and generate this serv

--- src/app/api/firebase/getProjects/serviceAccount.ts
+++ src/app/api/firebase/getProjects/serviceAccount.ts
@@ -3,17 +3,17 @@
 
 export const serviceAccount = {
   "type": "service_account",
-  "project_id": process.env.GCP_PROJECT_ID,
-  "private_key_id": process.env.GCP_PRIVATE_KEY_ID,
+  "project_id": process.env.FIREBASE_PROJECT_ID,
+  "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
   // The private key is sensitive and should be handled carefully.
   // The .replace() call is necessary to handle line breaks correctly when loading from a .env file.
-  "private_key": (process.env.GCP_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
-  "client_email": process.env.GCP_CLIENT_EMAIL,
-  "client_id": process.env.GCP_CLIENT_ID,
+  "private_key": (process.env.FIREBASE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
+  "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+  "client_id": process.env.FIREBASE_CLIENT_ID,
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": process.env.GCP_CLIENT_X509_CERT_URL
+  "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL
 };
 
 // Validate that the required environment variables are set.
