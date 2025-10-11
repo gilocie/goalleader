8f9ef7cd704ab8943ca6fa7bc7d26df87d7b6cd0
Gift Ilocie <gilocie@gmail.com>
2025-10-11 10:22:41 +0000

    reduce edit report hight card by 100px to make entire card fully visible

--- src/app/reports/reports-page-content.tsx
+++ src/app/reports/reports-page-content.tsx
@@ -215,7 +215,7 @@ export function ReportsPageContent() {
                     id="edit-content"
                     value={editedContent}
                     onChange={(e) => setEditedContent(e.target.value)}
-                    className="h-64"
+                    className="h-40"
                 />
             </div>
           </div>
