44599d5180ccc6db4191bd93b36016774611d0a6
Gift Ilocie <gilocie@gmail.com>
2025-10-15 15:55:17 +0000

    make sure that my firebase config is even geting AI API key fron .env

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,6 +3,12 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 import 'dotenv/config';
 
+if (!process.env.GEMINI_API_KEY) {
+  console.warn(
+    '\n\nWARNING: GEMINI_API_KEY is not defined. Please add it to your .env file.\n\n'
+  );
+}
+
 export const ai = genkit({
   plugins: [googleAI()],
 });
