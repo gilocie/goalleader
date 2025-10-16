509216afd45366179d376053487f0d4e03875c7e
Gift Ilocie <gilocie@gmail.com>
2025-10-16 13:18:22 +0000

    when i click call in chat i see dialog, when i click call in team page o

--- src/hooks/use-outgoing-call.ts
+++ src/hooks/use-outgoing-call.ts
@@ -5,15 +5,17 @@ import { useChat } from "@/context/chat-context";
 import type { Contact } from "@/types/chat";
 
 export const useOutgoingCall = () => {
-    const { startVoiceCall, startCall, setSelectedContact } = useChat();
+    const { startVoiceCall, startCall, setSelectedContact, setIsVoiceCallOpen, setIsVideoCallOpen } = useChat();
 
     const initiateVoiceCall = (contact: Contact) => {
         setSelectedContact(contact);
+        setIsVoiceCallOpen(true);
         startVoiceCall(contact);
     };
 
     const initiateVideoCall = (contact: Contact) => {
         setSelectedContact(contact);
+        setIsVideoCallOpen(true);
         startCall(contact);
     };
 
