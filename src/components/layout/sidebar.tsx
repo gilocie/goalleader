d5f65a4a6a387df98590828e171f73059e46cf0c
Gift Ilocie <gilocie@gmail.com>
2025-10-16 11:04:09 +0000

    Make this button squired (_for element <Button>_)

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -96,7 +96,7 @@ export function Sidebar() {
         variant="ghost"
         size="icon"
         className={cn(
-            "absolute top-[76px] -right-4 h-8 w-8 rounded-full bg-primary text-primary-foreground hover:bg-primary/90 z-50 transition-all duration-300",
+            "absolute top-[76px] -right-4 h-8 w-8 bg-primary text-primary-foreground hover:bg-primary/90 z-50 transition-all duration-300",
             !open && "-right-5"
         )}
         onClick={() => setOpen(!open)}
